45b47a3471f2021df0f847233aa8c16936dbc7cb
Akash Bangalkar <akash.bangalkar@flexagon.com>
2023-03-13 07:07:53 -0500

    Related List LWC

---
+++ lwc/accountRelatedList/accountRelatedList.js
@@ -0,0 +1,77 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import {getRelatedListRecords} from 'lightning/uiRelatedListApi';
+
+export default class AccountRelatedList extends LightningElement {
+    @api recordId;
+
+    //Contact Related Records
+    @track contactData;
+    @track contactError;
+    @wire(getRelatedListRecords, {
+        parentRecordId:'$recordId',
+        relatedRecordId:'Contacts',
+        field:['Contact.Id','Contact.Name','Contact.Title']
+    })
+    WiredContact(result)
+    {
+        const {data,error} = result;
+        if(data)
+        {
+            this.contactData = data.records;
+            this.contactError = undefined;
+        }
+        else if(error)
+        {
+            this.contactError = error;
+            this.contactData = undefined;
+        }
+    }
+
+    //Opportunity Related Records
+    @track oppData;
+    @track oppError;
+
+    @wire(getRelatedListRecords, {
+        parentRecordId:'$recordId',
+        relatedRecordId:'Opportunities',
+        field:['Opportunity.Id','Opportunity.Name','Opportunity.StageName']
+    })
+    WiredOpp(result)
+    {
+        const {data, error} = result;
+        if(data)
+        {
+            this.oppData = data.records;
+            this.oppError = undefined;
+        }
+        else if(error)
+        {
+            this.oppError = error;
+            this.oppData = undefined;
+        }
+    }
+
+    //Case Related Records
+    @track caseData;
+    @track caseError;
+
+    @wire(getRelatedListRecords, {
+        parentRecordId:'$recordId',
+        relatedRecordId:'Cases',
+        field:['Case.Id','Case.Number','Case.Subject']
+    })
+    WiredCase(result)
+    {
+        const {error,data} = result;
+        if(data)
+        {
+            this.caseData = data.records;
+            this.caseError = undefined;
+        }
+        else if(error)
+        {
+            this.caseError = error;
+            this.caseData = undefined;
+        }
+    }
+}
